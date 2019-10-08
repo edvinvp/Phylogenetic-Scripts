3cb195c949128385953224a6c7ec8ea4dfc09988
Edvin <edvinvp@kth.se>
2019-10-08 20:10:56 +0200

    Removed old scripts

--- dlrsanalysis.py
+++
@@ -1,5 +0,0 @@
-import treeanalysis as ta
-import duplicateanalysis as da
-
-da.duplication_analysis("NonRoot/traces/real/json_stats/")
-ta.tree_analysis("NonRoot/traces/real/json/", "NonRoot/sim_data/real/")
